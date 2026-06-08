5137127cd288670b74357ad6d0dc5db42b98c2db
Gonzalo Gomez <gomezgh91@gmail.com>
2026-06-08 15:02:13 -0300

    Mejorado manejo de errores, agregado newline en los argumentos de 'open' para evitar problemas con saltos de linea

--- tpi-paises-python/main.py
+++ tpi-paises-python/main.py
@@ -2,10 +2,14 @@ import csv
 
 ARCHIVO_CSV = 'paises.csv'
 
-#=== Mensajes de error ===#
-ERROR_ARCHIVO_NO_ENCONTRADO = f"Error: no se encontró el archivo {nombre_archivo}."
-ERROR_CSV_NUMERO_INVALIDO = "Error: el archivo CSV contiene valores numéricos inválidos."
-ERROR_CSV_COLUMNA_FALTANTE = "Error: el archivo CSV no contiene las columnas necesarias."
+
+#=== Mensajes de error ===
+ERROR_ARCHIVO_NO_ENCONTRADO = "Error: no se encontró el archivo {}."
+ERROR_CSV_COLUMNA_FALTANTE = "Error: falta la columna '{}' en el archivo CSV."
+ERROR_CSV_NUMERO_INVALIDO = "Error: el país '{}' tiene un valor numérico inválido en población o superficie."
+
+
+#=== Funciones ===
 
 def cargar_paises(nombre_archivo):
     """
@@ -17,26 +21,28 @@ def cargar_paises(nombre_archivo):
 
     try:
         
-        with open(nombre_archivo, mode='r', encoding='utf-8') as archivo: # Abrir el archivo CSV en modo lectura con codificación UTF-8
+        with open(nombre_archivo, mode='r', encoding='utf-8', newline='') as archivo: # Abrir el archivo CSV en modo lectura con codificación UTF-8
             lector = csv.DictReader(archivo) # Crear un lector de CSV que devuelve cada fila como un diccionario
-            for fila in lector: # Iterar sobre cada fila del CSV
-                # Crear un diccionario para el país con los datos de la fila, convirtiendo los valores numéricos a enteros
-                pais = {
-                    "nombre": fila["nombre"],
-                    "poblacion": int(fila["poblacion"]),
-                    "superficie": int(fila["superficie"]),
-                    "continente": fila["continente"]
-                }
-                paises.append(pais) # Agregar el diccionario del país a la lista de países
+            for fila in lector:
+                try:
+                    pais = {
+                        "nombre": fila["nombre"],
+                        "poblacion": int(fila["poblacion"]),
+                        "superficie": int(fila["superficie"]),
+                        "continente": fila["continente"]
+                    }
+
+                    paises.append(pais)
+
+                except ValueError:
+                    print(ERROR_CSV_NUMERO_INVALIDO.format(fila["nombre"]))
     
     except FileNotFoundError:
-        print(ERROR_ARCHIVO_NO_ENCONTRADO)
-    
-    except ValueError:
-        print(ERROR_CSV_NUMERO_INVALIDO)
+        print(ERROR_ARCHIVO_NO_ENCONTRADO.format(nombre_archivo))
+
     
     except KeyError as e:
-        print(ERROR_CSV_COLUMNA_FALTANTE)
+        print(ERROR_CSV_COLUMNA_FALTANTE.format(e))
     
     return paises
     
