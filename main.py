590cc3924a539560988bebf78ecf2361fca1f190
Gonzalo Gomez <gomezgh91@gmail.com>
2026-06-08 15:23:08 -0300

    Agregada deteccion de campo vacio y csv de prueba

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import csv  
 
-ARCHIVO_CSV = 'paises.csv'
+ARCHIVO_CSV = 'paises_prueba.csv'
 
 
 #=== Mensajes de error ===
 ERROR_ARCHIVO_NO_ENCONTRADO = "Error: no se encontró el archivo {}."
-ERROR_CSV_COLUMNA_FALTANTE = "Error: falta la columna '{}' en el archivo CSV."
-ERROR_CSV_NUMERO_INVALIDO = "Error: el país '{}' tiene un valor numérico inválido en población o superficie."
-
+ERROR_CSV_COLUMNA_FALTANTE = "Error: falta la columna {} en el archivo CSV."
+ERROR_CSV_NUMERO_INVALIDO = "Error: el país {} tiene un valor numérico inválido en población o superficie."
+ERROR_PAIS_CON_CAMPO_VACIO = "Error: el país {} tiene campos vacíos."
 
 #=== Funciones ===
+
+def tiene_campos_vacios(fila):
+    """
+    Verifica si una fila del CSV tiene algún campo vacío.
+    """
+
+    return (
+        fila["nombre"].strip() == "" or
+        fila["poblacion"].strip() == "" or
+        fila["superficie"].strip() == "" or
+        fila["continente"].strip() == ""
+    )
+
 
 def cargar_paises(nombre_archivo):
     """
@@ -24,12 +37,17 @@ def cargar_paises(nombre_archivo):
         with open(nombre_archivo, mode='r', encoding='utf-8', newline='') as archivo: # Abrir el archivo CSV en modo lectura con codificación UTF-8
             lector = csv.DictReader(archivo) # Crear un lector de CSV que devuelve cada fila como un diccionario
             for fila in lector:
+
                 try:
+                    if tiene_campos_vacios(fila):
+                        print(ERROR_PAIS_CON_CAMPO_VACIO.format(fila["nombre"]))
+                        continue
+
                     pais = {
-                        "nombre": fila["nombre"],
+                        "nombre": fila["nombre"].strip(),
                         "poblacion": int(fila["poblacion"]),
                         "superficie": int(fila["superficie"]),
-                        "continente": fila["continente"]
+                        "continente": fila["continente"].strip()
                     }
 
                     paises.append(pais)
